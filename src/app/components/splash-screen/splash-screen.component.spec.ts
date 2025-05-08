e4c2a6744bb57d9ba74257b0979030fdd8ee690b
Stijn van de Sande <stijnvandesande@ziggo.nl>
2025-05-08 17:12:15 +0200

    refactor: simplify SplashScreenComponent tests using Spectator

--- src/app/components/splash-screen/splash-screen.component.spec.ts
+++ src/app/components/splash-screen/splash-screen.component.spec.ts
@@ -1,23 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { createComponentFactory, Spectator } from '@ngneat/spectator';
 import { SplashScreenComponent } from './splash-screen.component';
 
 describe('SplashScreenComponent', () => {
-  let component: SplashScreenComponent;
-  let fixture: ComponentFixture<SplashScreenComponent>;
+  let spectator: Spectator<SplashScreenComponent>;
+  const createComponent = createComponentFactory(SplashScreenComponent);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [SplashScreenComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(SplashScreenComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should show logo and title', () => {
+    const logo = spectator.query('img.logo');
+    const title = spectator.query('h1.title');
+
+    expect(logo).toBeTruthy();
+    expect(title).toBeTruthy();
+
+    expect(logo?.getAttribute('src')).toBe('images/readly-logo.png');
+    expect(title?.textContent).toContain('Readly');
+  });
+
+  it('should hide splash screen after animation duration', () => {
+    jasmine.clock().install();
+    spectator.component.ngOnInit();
+
+    expect(spectator.component.showSplash).toBe(true);
+
+    jasmine.clock().tick((spectator.component as any).animationDuration);
+
+    expect(spectator.component.showSplash).toBe(false);
+    jasmine.clock().uninstall();
   });
 });
