fb8bdc3ef950fade2b475c5e809e139c1f72dfb2
Stijn van de Sande <stijnvandesande@ziggo.nl>
2025-05-12 16:35:30 +0200

    test: menu component

--- src/app/components/menu/menu.component.spec.ts
+++ src/app/components/menu/menu.component.spec.ts
@@ -1,23 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MenuComponent } from './menu.component';
+import { createRoutingFactory, Spectator } from '@ngneat/spectator';
 
 describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+  let spectator: Spectator<MenuComponent>;
+  const createComponent = createRoutingFactory(MenuComponent);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [MenuComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(MenuComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  beforeEach(() => spectator = createComponent());
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
+  });
+
+  it('should show menu items', () => {
+    const menuItems = spectator.queryAll('.menu-item');
+    const expectedItems = spectator.component.menuItems;
+
+    expect(menuItems.length).toBe(expectedItems.length);
+
+    menuItems.forEach((item, index) => {
+      expect(item.textContent).toContain(expectedItems[index].name);
+    });
   });
 });
